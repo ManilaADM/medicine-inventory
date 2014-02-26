277df54280740dd057373387ac9a002e771a447e
Brian Tuboro <Brian.Tuboro@NIIT-Tech.com>
2014-02-26 20:22:23 +0800

    Added login page Added login action Added login config
    Change-Id: Ic9895ea7c91adf1ea9d41ee03101b891db091292

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,12 +3,16 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.login;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-
+    
+    public static Result login() {
+    	return ok(login.render());
+    }
     
 }
