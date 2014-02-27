5acfe814ae6b726c15a2b921d5c4d54d9a1c49f3
Brian Tuboro <Brian.Tuboro@NIIT-Tech.com>
2014-02-27 19:24:48 +0800

    Added a user form
    Change-Id: If52f79843a9dadf55e7c2d63c37d3fcad002cea6

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import static play.data.Form.*;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 import views.html.login;
+import play.data.*;
+import models.User;
 
 public class Application extends Controller {
 
@@ -12,7 +16,26 @@ public class Application extends Controller {
     }
     
     public static Result login() {
-    	return ok(login.render());
+    	return ok(login.render(Form.form(Login.class)));
     }
+    
+    public static class Login {
+
+        public String email;
+        public String password;
+        
+	    public String validate() {
+	    	if (User.authenticate(email, password) == null) {
+	    		return "Invalid user or password";
+	    	}
+	    	return null;
+	    }
+    }
+    
+    public static Result authenticate() {
+        Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+        return ok();
+    }
+    
     
 }
