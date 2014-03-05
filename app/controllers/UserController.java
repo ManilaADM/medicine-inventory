fb5810a6c029edf631f67aaed31b52dbaf2e6afb
Brian Tuboro <Brian.Tuboro@NIIT-Tech.com>
2014-03-06 01:37:25 +0800

    Added showing of name on main html Refactoring Change-Id: I542fddbc4cd6678c57e72bb497e565f51698270f

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,49 +1,52 @@
 package controllers;
 
-import dao.JongoDAO;
+import models.LoginForm;
 import models.User;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import dao.JongoDAO;
 import views.html.login;
 
 public class UserController extends Controller {
 	
 	static JongoDAO<User> userDao = new JongoDAO<>(User.class);
+	static Form<LoginForm> loginForm = Form.form(LoginForm.class);
 	
 	public static Result login() {
-    	return ok(login.render(Form.form(Login.class)));
-    }
-    
-    public static class Login {
-
-        public String email;
-        public String password;
-        
-	    public String validate() {
-	    	if (UserController.authenticate(email, password)) {
-	    		return "Invalid user or password";
-	    	}
-	    	return null;
-	    }
+    	return ok(login.render(loginForm));
     }
     
     public static boolean authenticate(String email, String password) {
-    	if ((userDao.searchOne("email", email) == null) || (userDao.searchOne("password", password) == null))
+    	if ((currentUserEmail(email) == null) || (currentUserPassword(password) == null))
     	{
     		return true;
     	}
     	return false;
     }
+
+	private static User currentUserPassword(String password) {
+		return userDao.searchOne("password", password);
+	}
+
+	private static User currentUserEmail(String email) {
+		return userDao.searchOne("email", email);
+	}
+    
+    public static String name() {
+    	return session("name");
+    }
     
     public static Result authenticate() {
-        Form<Login> loginForm = Form.form(Login.class).bindFromRequest();
+        Form<LoginForm> loginForm = Form.form(LoginForm.class).bindFromRequest();
         
         if (loginForm.hasErrors()) {
             return badRequest(login.render(loginForm));
         } else {
+        	User user = currentUserEmail(loginForm.get().email);
             session().clear();
             session("email", loginForm.get().email);
+            session("name", user.name);
             return redirect(
                 routes.Application.index()
             );
