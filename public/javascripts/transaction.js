28bfa241ca44abd7ef6776f5615feaa3f735ad61
Julius <Julius.Cruz@NIIT-Tech.com>
2014-04-14 14:06:00 +0800

    Test commit

--- public/javascripts/transaction.js
+++ public/javascripts/transaction.js
@@ -73,8 +73,7 @@ function updateMedicineFields(medBrandAndGenericName, index)
 	});
 }
 
-//function to display the box 
-//test
+//function to display the box
 
 function showOverlayBox(id) {
 	//if box is not set to open then don't do anything
