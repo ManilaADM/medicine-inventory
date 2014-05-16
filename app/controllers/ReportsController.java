75b05777d2b58e67c29855e621bb97544d265a29
Julius <Julius.Cruz@NIIT-Tech.com>
2014-05-16 13:24:49 +0800

    Add medicine inventory report

--- app/controllers/ReportsController.java
+++ app/controllers/ReportsController.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import java.util.List;
+
+import models.Medicine;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
+import service.MedicineManager;
+import service.impl.MedicineManagerImpl;
+import views.html.medicineInventory;
 import views.html.reports;
 
 public class ReportsController extends Controller {
+	
+	private static MedicineManager medicineManager = new MedicineManagerImpl();
 
 	@Security.Authenticated(Secured.class)
     public static Result getReports() {
@@ -21,7 +29,9 @@ public class ReportsController extends Controller {
     }
     
     public static Result getMedSupInventory() {
-        return ok(reports.render());
+		List<Medicine> medicines = medicineManager.findAll();
+		return ok(medicineInventory.render("Medicine Inventory",medicines));
+//		return ok(reports.render());
     }
     
     public static Result getMedSupCritical() {
